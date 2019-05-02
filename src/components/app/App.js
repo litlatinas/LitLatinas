48cc3761463e59294ecaa055f90fe12609b23699
pickuppatterns <pickup.patterns@gmail.com>
2019-05-02 14:54:41 -0700

    styles play button for podcast episodes

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -2,6 +2,11 @@ import React from 'react';
 import Header from '../header/Header';
 import Footer from '../Footer/Footer';
 import AllPodcasts from '../allPodcasts/AllPodcasts';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faPlayCircle, faEnvelope, faKey } from '@fortawesome/free-solid-svg-icons';
+
+
+library.add(faPlayCircle, faEnvelope, faKey);
 
 export default function App() {
   return (
