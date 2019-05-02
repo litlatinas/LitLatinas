cae071a16dceaaf5ab1f4904321bbb2bd817da15
pickuppatterns <pickup.patterns@gmail.com>
2019-05-02 16:12:15 -0700

    updates footer css

--- src/components/MailingList/MailingList.js
+++ src/components/MailingList/MailingList.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import styles from './MailingList.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 export default function MailingList() {
   return (
-    <form className={styles.mailingList}>  
-      <label>Join our Mailing List</label>
-      <input type="text"  placeholder="name" value=""></input>
-      <input type="text" placeholder="email" value=""></input>
-      <button> SIGN UP</button>
-    </form>
+    <div className={styles.mailingList}>
+      <FontAwesomeIcon className={styles.envelope}icon="envelope" size="10x" color="white"/> 
+      <form className={styles.mailingListForm}>  
+        <input type="text" placeholder="email" value=""></input>
+        <button> JOIN OUR MAILING LIST</button>
+      </form> 
+    </div>
   );
 }
