128ef04508d696a88b1e6e54ac629fecf283f7c4
pickuppatterns <pickup.patterns@gmail.com>
2019-05-16 18:01:16 -0700

    reorganizes api driven assets off static

--- src/components/Podcasts/Podcast.js
+++ src/components/Podcasts/Podcast.js
@@ -2,34 +2,29 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import styles from './AllPodcasts.css';
 import PropTypes from 'prop-types';
-// import { getPodcastBySlug } from '../../services/contentfulApi';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faPlayCircle, faEnvelope, faKey } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+
+
+library.add(faPlayCircle, faEnvelope, faKey);
 
 function Podcast({ podcast }) {
-  
-  
-  
-  // componentDidMount() {
-  //   const { slug } = this.props.match.params;
-
-  //   getPodcastBySlug(slug)
-  //     .then(response => {
-  //       const post = response.items[0].fields;
-  //       this.setState({ ...post, isLoading: false });
-  //     });
-  // }
- 
-
+   
   return (
     <article className={styles.article}>
-      <h1 className={styles.episodeTitle}>
+      <div className={styles.articleCover}>
+        <img className={styles.episodeImage} src={podcast.image} />
+        <button className={styles.playButton}><FontAwesomeIcon className={styles.playIcon} icon="play-circle"/>             LISTEN</button>
+      </div>
+      <span><h1 className={styles.episodeTitle}>
         <Link to={`/podcasts/${podcast.slug}`}>
           {podcast.title}
         </Link>
-      </h1>
-        
-      {/* <span>
-          {moment(datePublished).format('MMMM D, YYYY')}
-        </span> */}
+      </h1></span>
+      <h3 className={styles.episodeDate}>{podcast.datePublished}</h3>
+      <p className={styles.episodeDescription}>{podcast.description}</p> 
     </article>
   );
 }
