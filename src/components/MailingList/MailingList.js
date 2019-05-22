ad0a1292cb32c1cd569b5d27930c2bd3b2146a84
TEONNA ZARAGOZA <teonna.zaragoza@gmail.com>
2019-05-22 14:22:09 -0700

    small changes

--- src/components/MailingList/MailingList.js
+++ src/components/MailingList/MailingList.js
@@ -1,23 +1,21 @@
 import React from 'react';
 import styles from './MailingList.css';
-import { library } from '@fortawesome/fontawesome-svg-core'
+import { library } from '@fortawesome/fontawesome-svg-core';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
 
 
-library.add(faEnvelope)
-
-
+library.add(faEnvelope);
 
 
 export default function MailingList() {
   return (
     <div className={styles.mailingList}>
-      <FontAwesomeIcon className={styles.envelope} icon="envelope" size="7x"/> 
-      <form className={styles.mailingListForm}>  
+      <FontAwesomeIcon className={styles.envelope} icon="envelope" size="7x" />
+      <form className={styles.mailingListForm}>
         <input type="text" placeholder="email" value=""></input>
         <button> JOIN OUR MAILING LIST</button>
-      </form> 
+      </form>
     </div>
   );
 }
