eac2da5c400046c6e433644cdea29f9103890ef9
TEONNA ZARAGOZA <teonna.zaragoza@gmail.com>
2019-05-22 12:24:08 -0700

    added a tag

--- src/components/Resources/Resource.js
+++ src/components/Resources/Resource.js
@@ -2,11 +2,12 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 function Resource({ resource }) {
+  console.log('resourcewebs', resource.website);
   return (
     <>
       <h2>{resource.resourceTitle}</h2>
       <p>{resource.body}</p>
-      <p>{resource.website}</p>
+      <a href={resource.website} target="_blank" rel="noopener noreferrer">{resource.website}</a>
     </>
   );
 }
