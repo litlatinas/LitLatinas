0a6f410952ebffe86b43fcfd7c6ec80217221585
Michael <Fieldnotes@hotrocks-MacBook.local>
2019-05-24 09:03:08 -0700

    styles resources page assets

--- src/components/Resources/Resource.js
+++ src/components/Resources/Resource.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import styles from './Resource.css';
 
 function Resource({ resource }) {
   console.log('resourcewebs', resource.website);
   return (
-    <>
+    <section className={styles.section}>
       <h2>{resource.resourceTitle}</h2>
       <p>{resource.body}</p>
       <a href={resource.website} target="_blank" rel="noopener noreferrer">{resource.website}</a>
-    </>
+    </section>
   );
 }
 Resource.propTypes = {
