1144a3f69b56ae84692297fa81a373bfddb99f72
TEONNA ZARAGOZA <teonna.zaragoza@gmail.com>
2020-06-17 13:02:45 -0700

    try

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render } from 'react-dom';
-import App from './components/app/app';
+import App from './components/app/app.js';
 import 'normalize.css';
 
 render(
