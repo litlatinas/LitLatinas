1399822b8e5408c7219e1b16ee1c77bfbfaad0c6
pickuppatterns <pickup.patterns@gmail.com>
2020-06-27 14:03:03 -0700

    adds client method to query the api for podcasts

--- src/services/prismicApi.js
+++ src/services/prismicApi.js
@@ -2,7 +2,29 @@ import Prismic from 'prismic-javascript';
 
 import { prismicApiUrl } from '../utils/constants';
  
+const apiEndpoint = 'https://litlatinas.cdn.prismic.io/api/v2';
 const Client = Prismic.client(prismicApiUrl);
+
+client.query(
+  [Prismic.Predicates.at('document.type', 'title'), 
+    Prismic.Predicates.at('document.type', 'slug'),
+    Prismic.Predicates.at('document.type', 'date_published'),
+    Prismic.Predicates.at('document.type', 'podcast'),
+    Prismic.Predicates.at('document.type', 'description'),
+    Prismic.Predicates.at('document.type', 'body'),
+    Prismic.Predicates.at('document.type', 'image'),
+    Prismic.Predicates.at('document.type', 'resource'),
+    Prismic.Predicates.at('document.type', ['featured'])]
+  //ordering options and pageSize or pages exist search how to query api
+  //{ orderings : '[my.blog_post.date desc]' }
+  //{ pageSize : 10, page : 2 }
+).then(response => {
+  response.results; 
+});
+
+export const client = Prismic.client(apiEndpoint, { accessToken });
+
+
 
 export const prismicGetter = async(type) => {
   return Client.query(
