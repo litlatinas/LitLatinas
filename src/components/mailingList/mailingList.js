40217146646ab2bd0e0a9bbabd6e4b7e6b4e2284
pickuppatterns <pickup.patterns@gmail.com>
2020-06-26 17:06:23 -0700

    clears form after click

--- src/components/mailingList/mailingList.js
+++ src/components/mailingList/mailingList.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import propTypes from 'prop-types';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
@@ -11,6 +12,7 @@ library.add(faEnvelope);
 const Form = ({ status, message, onValidated }) => {
   let email, name;
   
+
   const submit = (e) => {
     email &&
     name &&
@@ -20,47 +22,64 @@ const Form = ({ status, message, onValidated }) => {
       NAME: name.value
     });
     e.preventDefault();
+    document.getElementById('mailchimpForm').reset();
+    
   };
     
   
 
   return (
-    <div className={styles.mcSignup}>
+    <form id='mailchimpForm' className={styles.mcSignup}>
       <FontAwesomeIcon className={styles.envelope} icon="envelope" size="7x" />
       <div className={styles.mailingListForm}>
         <input type="email" placeholder="email" ref={node => (email = node)} />
         <input type="text" placeholder="name" ref={node => (name = node)} />
-        <button onClick={submit}>
+        <button type="submit" onClick={submit}>
           JOIN OUR MAILING LIST
         </button>
-        <div className={styles.mailingList}>
-          {status === 'sending' && <div style={{ color: 'blue' }}>sending...</div>}
-          {status === 'error' && 
-            <div className={styles.mcResponse}
-              style={{ color: 'red' }}
-            >OOPS ! SOMETHING WENT WRONG, TRY AGAIN ?</div>}
-          {status === 'success' && 
-            <div className={styles.mcResponse}
-              style={{ color: 'green' }}
-            >SUBSCRIBED! </div>}
-        </div >
+        <div id='response'>
+          <div className={styles.mailingList}>
+            {status === 'sending' && 
+              <div className={styles.mcResponse} 
+                style={{ color: 'blue' }}>SENDING...</div>}
+            {status === 'error' && (
+              <div className={styles.mcResponse}
+                style={{ color: 'red' }}
+                dangerouslySetInnerHTML={{ __html: message }}
+              />
+            )}
+            {status === 'success' && (
+              <div className={styles.mcResponse}
+                style={{ color: 'green' }}
+              >THANK YOU FOR SUBSCRIBING TO THE LIT LATINAS MAILING LIST! </div>
+            )}
+          </div>
+        </div>
       </div>
-    </div>
+    </form>
   );
 };
 
+Form.propTypes = {
+  status: propTypes.string.isRequired,
+  message: propTypes.string.isRequired,
+  onValidated: propTypes.object.isRequired,
+};
 class MailingList extends Component {
+  
   render() {
     const url = 'https://gmail.us20.list-manage.com/subscribe/post?u=e8e2a5bc28b9a9cee48f6d372&amp;id=e37f71128d';
+
     return (
       <div>
         <MailchimpSubscribe
           url={url}
           render={({ subscribe, status, message }) => (
-            <Form
+            <Form 
               status={status}
               message={message}
               onValidated={formData => subscribe(formData)}
+
             />
           )}
         />
