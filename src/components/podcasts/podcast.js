52783cfc4c8a03155e968874559e7ba7392bbae6
pickuppatterns <pickup.patterns@gmail.com>
2020-07-02 15:00:37 -0700

    useState now renders dynamic url in a button click unique to each podcast entry

--- src/components/podcasts/podcast.js
+++ src/components/podcasts/podcast.js
@@ -1,34 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPlayCircle } from '@fortawesome/free-solid-svg-icons';
 import moment from 'moment';
 import ResponsivePlayer from '../responsivePlayer/responsivePlayer';
-
 import styles from './podcast.css';
 
 library.add(faPlayCircle);
 
 function Podcast({ item }) {
+
+  const [url, setUrl] = useState(item?.enclosure.url);
+  const payload = () => {
+    setUrl(//should hoist a new url but how do i pass to responsivePlayer component//
+    );
+  };
+
   return (
     <article className={styles.article}>
       <h3 className={styles.episodeTitle}>{item?.title}</h3>
       <div className={styles.articleCover}>
         <img className={styles.episodeImage} src={item?.itunes?.image} alt="podcast-art"/>
         <button className={styles.playButton}
-          onClick= {ResponsivePlayer}
+          onClick= {payload} url={url}
         >
           <FontAwesomeIcon className={styles.playCircle} icon="play-circle" /> LISTEN</button>
       </div>
+      <h4>{item?.enclosure?.url}</h4>
       <h4>{item?.itunes?.duration}</h4>
       <h4>{moment(item?.pubDate).format('MMMM D, YYYY')}</h4>
       <p>{item?.contentSnippet}</p>
     </article>
   );
 }
+
+
 Podcast.propTypes = {
   item: PropTypes.object
 };
-
 export default Podcast;
--- src/components/responsivePlayer/responsivePlayer.js
+++ src/components/responsivePlayer/responsivePlayer.js
@@ -4,21 +4,20 @@ import PropTypes from 'prop-types';
 import styles from './responsivePlayer.css';
 
 
-const ResponsivePlayer = ({ item }) =>  {
+const ResponsivePlayer = ({ url }) =>  {
   return (
     <div className={styles.playerContents}>
       <ReactPlayer
         className={styles.reactPlayer}
-        url={item?.enclosure?.url}
+        url={url}
         width='100%'
         height='100%' 
         controls={true}
       />
     </div>  
   );
-  
 };
 ResponsivePlayer.propTypes = {
-  item: PropTypes.object
+  url: PropTypes.string
 };
 export default ResponsivePlayer;
