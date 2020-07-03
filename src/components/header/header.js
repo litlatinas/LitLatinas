4146c2645d387f8ce2271ffb75be0782c53535bb
pickuppatterns <pickup.patterns@gmail.com>
2020-07-02 18:52:53 -0700

    strips normalize css and updates styles in multiple docs

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -7,10 +7,10 @@ export default function Header() {
   return (
     <header className={styles.header}>  
       <h2>Lit • Latinas</h2>
+      <button> SUPPORT US</button>
       <h3>with your hosts Loerit &amp; Karen </h3>
       <h4>Welcome to Lit Latinas, a social action podcast that researches, reviews, &amp; critiques marginalized communities, mental health, social work, access to education, continued learning, and community.</h4>
-      <Nav className={styles.nav} />
-      <button> SUPPORT US</button>
+      {/* <Nav className={styles.nav} /> */}
     </header>
   );
 }
