0708c796fb1292f95ffb521d91c5bae7952bafa6
TEONNA ZARAGOZA <teonna.zaragoza@gmail.com>
2020-07-08 21:39:58 -0700

    WIP

--- src/components/podcasts/podcastList.js
+++ src/components/podcasts/podcastList.js
@@ -1,32 +1,45 @@
-import React, { useState, useEffect } from 'react';
-import RSSParser from 'rss-parser';
-import Podcast from './podcast';
-import styles from './podcastList.css';
+import React, { useState, useEffect } from "react";
+import RSSParser from "rss-parser";
+import Podcast from "./podcast";
+import styles from "./podcastList.css";
+import ResponsivePlayer from "../responsivePlayer/responsivePlayer";
 
 const PodcastList = () => {
-  const [feed, setFeed] = useState({ 
-    title:'',
-    items:[]
+  const [feed, setFeed] = useState({
+    title: "",
+    items: [],
   });
-  const rssData = async() => {
-    const CORS_PROXY = 'https://cors-anywhere.herokuapp.com/';
+  const rssData = async () => {
+    const CORS_PROXY = "https://cors-anywhere.herokuapp.com/";
     let parser = new RSSParser();
 
     try {
-      const feed = await parser.parseURL(`${CORS_PROXY}https://feeds.soundcloud.com/users/soundcloud:users:484400916/sounds.rss`);
+      const feed = await parser.parseURL(
+        `${CORS_PROXY}https://feeds.soundcloud.com/users/soundcloud:users:484400916/sounds.rss`,
+      );
       setFeed(feed);
-    } catch(error) {
+    } catch (error) {
       console.log(error);
     }
   };
+
   useEffect(() => {
     rssData();
   }, []);
 
-  const rssPodcasts = feed.items.map((item, i) => <div key={i}> <Podcast item={item} /></div>);
-  
+  let [url, setUrl] = useState({ url: '', setUrl: 'https://www.youtube.com/watch?v=KgBm7rhUMXU&list=RDKgBm7rhUMXU&start_radio=1' });
+
+  const rssPodcasts = feed.items.map((item, i) => {
+    const handleClick = () => {
+      setUrl = item?.enclosure?.url;
+    };
+
+    return <div key={i}><Podcast item={item} onClick={handleClick} /></div>;
+  });
+
   return (
     <>
+      <ResponsivePlayer url={url}/>
       <section id="podcasts">
         <h4>EPISODES:</h4>
         <ul className={styles.podcastList}>
--- src/components/responsivePlayer/responsivePlayer.js
+++ src/components/responsivePlayer/responsivePlayer.js
@@ -4,7 +4,8 @@ import PropTypes from 'prop-types';
 import styles from './responsivePlayer.css';
 
 
-const ResponsivePlayer = ({ url }) =>  {
+const ResponsivePlayer = ({ url }) => {
+  console.log('url: ', url);
   return (
     <div className={styles.playerContents}>
       <ReactPlayer
